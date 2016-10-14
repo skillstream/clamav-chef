5333ee13ea6c20b62201019f519343ef30ab1b55
Jonathan Freedman <jonathan.freedman@autodesk.com>
2016-10-14 12:05:41 -0700

    Version bumped to 1.3.900

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ maintainer_email 'j@p4nt5.com'
 license 'Apache v2.0'
 description 'Installs/configures ClamAV'
 long_description 'Installs/configures ClamAV'
-version '1.3.1'
+version '1.3.900'
 chef_version '>= 12'
 
 source_url 'https://github.com/roboticcheese/clamav-chef'
