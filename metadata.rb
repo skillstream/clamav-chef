d859a1796d68c4e94e616a955734bc84963019df
Dave Gudge <dave@skillstream.co.uk>
2017-08-18 12:07:11 +0100

    fix: < 12.6 chef compatibility
    Remove dependency versions

--- metadata.rb
+++ metadata.rb
@@ -8,18 +8,18 @@ license 'Apache v2.0'
 description 'Installs/configures ClamAV'
 long_description 'Installs/configures ClamAV'
 version '1.3.900'
-chef_version '>= 12'
+chef_version '>= 12' if respond_to?(:chef_version)
 
 source_url 'https://github.com/roboticcheese/clamav-chef'
 issues_url 'https://github.com/roboticcheese/clamav-chef/issues'
-depends           'logrotate', '~> 1.0'
-depends           'yum', '~> 3.0'
-depends           'yum-epel', '~> 0.2'
-depends           'apt', '~> 2.1'
+depends           'logrotate'
+depends           'yum'
+depends           'yum-epel'
+depends           'apt'
 depends           'poise-service'
 # Note that a breaking bug was introduced in 1.3.10 and fixed in 1.3.12, but
 # we really don't want a ">=" cookbook dep situation here
-depends           'cron', '~> 1.2'
+depends           'cron'
 
 supports 'ubuntu'
 supports 'debian'
